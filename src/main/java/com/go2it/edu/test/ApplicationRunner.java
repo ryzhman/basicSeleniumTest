db3434899856f42d113d3a82311cc6df0e72bed8
Oleksandr Ryzhkov <oryzhkov@paymentus.com>
2019-04-17 23:57:54 -0400

    Basic configuration for Selenium based test of Google search page; basic dependencies.

---
+++ src/main/java/com/go2it/edu/test/ApplicationRunner.java
@@ -0,0 +1,29 @@
+package com.go2it.edu.test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * @author Alex Ryzhkov
+ */
+public class ApplicationRunner {
+	public static void main(String[] args) throws InterruptedException {
+		//TODO replace the path to geckodriver.exe (Google where to download the latest version)
+		System.setProperty("webdriver.gecko.driver",
+				"C:\\Users\\oryzhkov\\Downloads\\geckodriver-v0.24.0-win64\\geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		driver.get("http://www.google.com");
+		WebElement element = driver.findElement(By.name("q"));
+		element.sendKeys("Go2IT.ca java");
+		element.submit();
+
+		WebDriver.Options manage = driver.manage();
+		synchronized (manage) {
+			manage.wait(5000);
+		}
+		;
+		driver.close();
+	}
+}
